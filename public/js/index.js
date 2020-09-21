a4bce9303f6367c7f7661da43616fd47148ee0ce
amruta <mulikamruta.94@gmail.com>
2020-09-21 18:00:56 +0530

    added custom/broadcasting events

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+let socket = io();
+
+socket.on('connect',function () {
+    console.log('Connected to server');
+
+   // socket.emit('createMessage', {"from":"wdj", "text":"wats going on"});
+
+});
+
+socket.on('disconnect',function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message){
+    console.log('newMessage',message);
+});
--- server/server.js
+++ server/server.js
@@ -15,9 +15,34 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=> {
     console.log("A new user just connected");
 
+    //welcome message to new user
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the chat app!",
+        createdAt: new Date().getTime()
+    });
+
+    //brodcast to everyone except the new user
+    socket.broadcast.emit('newMessage', { 
+        from : "Admin" , 
+        text : "New user Joined!", 
+        createdAt : new Date().getTime()
+    });
+
+    socket.on('createMessage', (message)=> {
+        console.log("CreateMessage :", message);
+        
+        io.emit('newMessage', { 
+            from : message.from , 
+            text : message.text, 
+            createdAt : new Date().getTime()
+        });
+
+    });
+
     socket.on('disconnect',()=> {
         console.log('User was disconnected');
-      });
+    });
     
 })
 
