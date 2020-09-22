fbd77b69925a074feef66e586bd6510f3354dced
amruta <mulikamruta.94@gmail.com>
2020-09-22 13:46:01 +0530

    final

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,6 +9,14 @@ socket.on('connect', function() {
   let searchQuery = window.location.search.substring(1);
   let params = JSON.parse('{"' + decodeURI(searchQuery).replace(/&/g, '","').replace(/\+/g, ' ').replace(/=/g,'":"') + '"}');
 
+  // chat__sidebar
+  let sidebar = document.querySelector('#chat__sidebar');
+  sidebar.innerHTML = "";
+  let title = document.createElement('h3');
+  title.innerHTML = params.name;
+
+  sidebar.appendChild(title);
+
   socket.emit('join', params, function(err) {
     if(err){
       alert(err);
@@ -17,25 +25,14 @@ socket.on('connect', function() {
       console.log('No Error');
     }
   })
+
 });
 
 socket.on('disconnect', function() {
   console.log('disconnected from server.');
 });
 
-socket.on('updateUsersList', function (users) {
-  let ol = document.createElement('ol');
 
-  users.forEach(function (user) {
-    let li = document.createElement('li');
-    li.innerHTML = user;
-    ol.appendChild(li);
-  });
-
-  let usersList = document.querySelector('#users');
-  usersList.innerHTML = "";
-  usersList.appendChild(ol);
-})
 
 socket.on('chat', function(msg) {
   let message = JSON.parse(msg);
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 var redis = require('redis');
 const moment = require('moment');
-const {Users} = require('./utils/users');
+const {Users} = require('./model/users');
 
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000;
